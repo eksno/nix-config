bfe7e72c88b0c3a79c28d68569754d2b56119376
Jonas Lindberg <eksno@protonmail.com>
2023-09-14 22:43:11 +0200

    forgor some stuff

--- configuration.nix
+++ configuration.nix
@@ -117,9 +117,6 @@
     podman = {
       enable = true;
 
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
       # For Nixos version > 22.11
@@ -129,7 +126,7 @@
     };
     docker = {
       enable = true;
-    }
+    };
   };
 
   # K3S
