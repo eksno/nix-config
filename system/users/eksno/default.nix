e5d536bd3ba5e30c7884d878daf4bdde4af76178
Jonas Lindberg <eksno@protonmail.com>
2023-10-09 18:28:38 +0200

    openssh on eksno

--- system/users/eksno/default.nix
+++ system/users/eksno/default.nix
@@ -4,6 +4,15 @@
   users.users.eksno = {
     isNormalUser = true;
     description = "Jonas Lindberg";
-    extraGroups = [ "networkmanager" "wheel" "video" ];
+    extraGroups = [ "networkmanager" "wheel" "video" "docker" ];
+  };
+
+  virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
+
+  services.openssh = {
+    enable = true;
   };
 }
