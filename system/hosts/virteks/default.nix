2e246312cb9f4e026dc30f3485a852b83d4e587f
Jonas Lindberg <eksno@protonmail.com>
2023-10-10 13:03:11 +0200

    virtualbox fixes maybe

--- system/hosts/virteks/default.nix
+++ system/hosts/virteks/default.nix
@@ -16,5 +16,18 @@
   environment.sessionVariables = {
     LIBGL_ALWAYS_SOFTWARE="true";
     GALLIUM_DRIVER="llvmpipe";
+    GDK_SCALE="1";
+  };
+
+  systemd.services.virtualbox-resize = {
+    description = "VBox resizing";
+
+    wantedBy = [ "multi-user.target" ];
+    requires = [ "dev-vboxguest.device" ];
+    after = [ "dev-vboxguest.device" ];
+
+    unitConfig.ConditionVirtualization = "oracle";
+
+    serviceConfig.ExecStart = "@${config.boot.kernelPackages.virtualboxGuestAdditions}/bin/VBoxClient -fv --vmsvga";
   };
 } 
