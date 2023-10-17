2bdaa3464b188642fad20bd23f44fa8b428419ad
Daniel Aanensen <calibor@protonmail.com>
2023-10-18 01:37:51 +0200

    pkg section on calibor

--- home/users/calibor/default.nix
+++ home/users/calibor/default.nix
@@ -11,4 +11,9 @@
   programs.git.userName = "Daniel Aanensen";
   programs.git.userEmail = "calibor@protonmail.com";
   programs.git.extraConfig.github.user = "calibor";
+
+
+  home.packages = with pkgs; [
+
+  ];
 }
