169d9564f7fb9ae60963e665aa40825e753d22f2
Daniel Aanensen <calibor@protonmail.com>
2023-11-26 21:53:55 +0100

    nvim and windowrules on webcord

--- home/users/lucy/hypr/nvim/lua/config/keymaps.lua
+++ home/users/lucy/hypr/nvim/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- vim.keymap.set( "n", ("<C-d>", "<C-d>zz"))
+-- vim.keymap.set( "n", ("<C-u>", "<C-u>zz"))
+-- nnoremap("<C-d>", "<C-d>zz")
+-- nnoremap("<C-u>", "<C-u>zz")
+-- map( "n", "<C-d>", "<C-d>zz" { desc = "Go to left window", remap = true })
+-- map( "n", "<C-u>", "<C-u>zz" { desc = "Go to left window", remap = true })
+
+vim.keymap.set('n', '<leader>sv', ':source $MYVIMRC<CR>')
+
+vim.keymap.set({'n', 'v'}, '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set({'n', 'v'}, '<C-u>', '<C-u>zz', { noremap = true })
+
+
+vim.keymap.set('n', '<leader>e', ':E<CR>', {noremap = true})
+
+
