9dfe6d63abd02a87f093d3831ca76f3ac07a9b6a
Daniel Aanensen <calibor@protonmail.com>
2023-11-28 22:16:56 +0100

    remove sway and update tofi for lucy

--- home/users/lucy/dotfiles/symlink.sh
+++ home/users/lucy/dotfiles/symlink.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
 remove() {
+	# Configs
 	rm -rf ~/.config/hypr
 	rm -rf ~/.config/nvim
 	rm -rf ~/.config/eww
 	rm -rf ~/.config/uwu-pics/
+	rm -rf ~/.config/tofi
+
+	# Fonts
+	rm -rf ~/.local/share/fonts
 }
 
 symlink() {
-	mkdir ~/.config/
+	mkdir -p ~/.config/
+
+	# Configs
 	ln -s ~/nix-config/home/users/lucy/dotfiles/hypr/ ~/.config/hypr
 	ln -s ~/nix-config/home/users/lucy/dotfiles/nvim/ ~/.config/nvim
 	ln -s ~/nix-config/home/users/lucy/dotfiles/eww ~/.config/eww
 	ln -s ~/uwu-pics/ ~/.config/uwu-pics
+	ln -s ~/nix-config/dotfiles/tofi ~/.config/tofi
+
+	# Fonts
+	ln -s /run/current-system/sw/share/X11/fonts ~/.local/share/fonts
 }
 
 remove
