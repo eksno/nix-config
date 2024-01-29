f98e6360b18c92764ef68119f72d777fe9cdc90c
Jonas Lindberg <eksno@protonmail.com>
2024-01-29 15:34:44 +0100

    catppucchin

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set('n', '<leader>sv', ':source $home/.config/nvim/init.lua <cr>')
+vim.keymap.set('n', '<leader>sv', ':source ~/nix-config/dotfiles/nvim/init.lua <cr>')
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
