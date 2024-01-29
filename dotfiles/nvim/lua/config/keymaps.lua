f9165bd099546801f4b12df1fb494fc626fcf203
Jonas Lindberg <eksno@protonmail.com>
2024-01-29 14:57:45 +0100

    rework the keybinds, much better

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -4,5 +4,81 @@
 
 vim.keymap.set('n', '<leader>sv', ':source $home/.config/nvim/init.lua <cr>')
 
-vim.keymap.set({'n', 'v'}, '<C-d>', '<C-d>zz', { noremap = true })
-vim.keymap.set({'n', 'v'}, '<C-u>', '<C-u>zz', { noremap = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set({'n', 'v'}, '<C-u>', '<C-u>zz')
+vim.keymap.set({'n', 'v'}, '<C-d>', '<C-d>zz')
+
+vim.keymap.set({'n', 'v'}, 'n', 'nzzzv')
+vim.keymap.set({'n', 'v'}, 'N', 'nzzzv')
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-f>", function()
+    vim.lsp.buf.format()
+end)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<C-c>", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader>xr", "<cmd>!chmod +x %<CR>", { desc = "Make the current file executable", silent = true })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
