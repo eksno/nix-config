a623c10d3b08aa66faefab0482147c0038acda8b
Jonas Lindberg <eksno@protonmail.com>
2024-02-01 13:54:01 +0100

    tmux better sessions

--- system/shared/fish.nix
+++ system/shared/fish.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 {
-  users.defaultUserShell = pkgs.fish;
-  environment.shells = with pkgs; [ fish ];
-  programs = {
-    fish = {
-      enable = true;
-      interactiveShellInit = "tmux";
+    users.defaultUserShell = pkgs.fish;
+    environment.shells = with pkgs; [ fish ];
+    programs = {
+        fish = {
+            enable = true;
+            interactiveShellInit = "tmux attach -t $USER || tmux new -s $USER";
+        };
     };
-  };
 }
