f3d53b8f15d9136fc108f0211a6ba39a2084e46c
Jonas Lindberg <eksno@protonmail.com>
2024-02-12 16:05:30 +0100

    update nvim keymaps

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -2,18 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set('n', '<leader>sv', ':source ~/nix-config/dotfiles/nvim/init.lua <cr>')
+vim.keymap.set("n", "<leader>sv", ":source ~/nix-config/dotfiles/nvim/init.lua <cr>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 
-vim.keymap.set({'n', 'v'}, '<C-u>', '<C-u>zz')
-vim.keymap.set({'n', 'v'}, '<C-d>', '<C-d>zz')
+vim.keymap.set({"n", "v"}, "<C-u>", "<C-u>zz")
+vim.keymap.set({"n", "v"}, "<C-d>", "<C-d>zz")
 
-vim.keymap.set({'n', 'v'}, 'n', 'nzzzv')
-vim.keymap.set({'n', 'v'}, 'N', 'nzzzv')
+vim.keymap.set({"n", "v"}, "n", "nzzzv")
+vim.keymap.set({"n", "v"}, "N", "nzzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
@@ -25,6 +25,8 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", function()
     vim.lsp.buf.format()
 end)
+
+vim.keymap.set("n", "<C-l>", "<leader>e")
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
