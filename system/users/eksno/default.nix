3a307cc16ca9d1ce2624d9b8980a3dc4d2f3f77e
Jonas Lindberg <eksno@protonmail.com>
2024-02-25 20:57:12 +0100

    add ld library definition

--- system/users/eksno/default.nix
+++ system/users/eksno/default.nix
@@ -1,36 +1,41 @@
 
 { config, pkgs, ... }:
 {
-  imports = [
-    ../../shared/desktop/wayland/hyprland
-  ];
-
-  users.users.eksno = {
-    isNormalUser = true;
-    description = "Jonas Lindberg";
-    extraGroups = [ "networkmanager" "wheel" "video" "docker" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4AP87WBXD5W+VrUSNVNSc3UugdxtaMqenVlwApZAcf eksno" # content of authorized_keys file
-      # note: ssh-copy-id will add user@your-machine after the public key
-      # but we can remove the "@your-machine" part
+    imports = [
+        ../../shared/desktop/wayland/hyprland
     ];
-  };
 
-  services.xserver.xkb.layout = "us";
-  services.xserver.layout = "us";
-  services.xserver.xkb.variant = "dvp";
-  services.xserver.xkbVariant = "dvp";
-  console.useXkbConfig = true;
-  
-  services.xserver.displayManager.sddm.settings.Autologin.User = "eksno";
+    users.users.eksno = {
+        isNormalUser = true;
+        description = "Jonas Lindberg";
+        extraGroups = [ "networkmanager" "wheel" "video" "docker" ];
+        openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4AP87WBXD5W+VrUSNVNSc3UugdxtaMqenVlwApZAcf eksno" # content of authorized_keys file
+        # note: ssh-copy-id will add user@your-machine after the public key
+        # but we can remove the "@your-machine" part
+        ];
+    };
 
-  services.openssh.enable = true;
+    services.xserver.xkb.layout = "us";
+    services.xserver.layout = "us";
+    services.xserver.xkb.variant = "dvp";
+    services.xserver.xkbVariant = "dvp";
+    console.useXkbConfig = true;
+    
+    services.xserver.displayManager.sddm.settings.Autologin.User = "eksno";
 
-  virtualisation.docker.enable = true;
+    services.openssh.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    docker-compose
-    xorg.xmodmap
-    xorg.xkbcomp
-  ];
+    virtualisation.docker.enable = true;
+
+    programs.nix-ld.enable = true;
+    programs.nix-ld.libraries = with pkgs; [
+
+    ];
+
+    environment.systemPackages = with pkgs; [
+        docker-compose
+        xorg.xmodmap
+        xorg.xkbcomp
+    ];
 }
