f915fab79a2664f6b769d2fc353215a667f5962b
Jonas Lindberg <eksno@protonmail.com>
2024-06-21 14:49:46 +0700

    fix: fonts

--- system/shared/fonts.nix
+++ system/shared/fonts.nix
@@ -1,26 +1,23 @@
 { config, pkgs, ... }:
 {
-    fonts = {
-        fontDir.enable = true;
-        packages = with pkgs; [
-            nerdfonts
-            noto-fonts
-            noto-fonts-cjk
-            noto-fonts-emoji
-            liberation_ttf
-            fira-code
-            fira-code-symbols
-            mplus-outline-fonts.githubRelease
-            dejavu_fonts
-            dina-font
-            proggyfonts
-        ];
-        fontconfig = {
-            defaultFonts = {
-                serif = [ "Ubuntu" ];
-                sansSerif = [ "Ubuntu" ];
-                monospace = [ "Ubuntu" ];
-            };
+    fonts.fontDir.enable = true;
+    fonts.packages = with pkgs; [
+        (nerdfonts.override { fonts = [ "FiraCode" ]; })
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        liberation_ttf
+        fira-code
+        fira-code-symbols
+        mplus-outline-fonts.githubRelease
+        dina-font
+        proggyfonts
+    ];
+    fonts.fontconfig = {
+        defaultFonts = {
+            serif = [  "Liberation Serif" "Vazirmatn" ];
+            sansSerif = [ "Ubuntu" "Vazirmatn" ];
+            monospace = [ "Ubuntu Mono" ];
         };
     };
 }
