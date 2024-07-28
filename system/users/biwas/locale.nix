8a4d90f58ac63c86f4517da9cd5e334348007492
Biwas Bhandari <biwas2059@gmail.com>
2024-07-28 12:40:35 +0545

    fix(biwas): timezone

--- system/users/biwas/locale.nix
+++ system/users/biwas/locale.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   # Set your time zone.
-  time.timeZone = "Asia/Nepal";
+  time.timeZone = "Asia/Kathmandu";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
