b8870d2292836d78602175db63f051d88e575529
Jonas Lindberg <eksno@protonmail.com>
2024-09-05 01:19:03 +0700

    feat(programs): obisidan is back

--- home/shared/desktop/programs.nix
+++ home/shared/desktop/programs.nix
@@ -15,7 +15,7 @@
     libreoffice
     helvum
     dbeaver-bin
-    # obsidian # Update R.I.P <-- Update WE'RE SO BACK <-- I'm sorry little one, you were too trash for me to try to figure out. https://github.com/NixOS/nixpkgs/issues/302457
+    obsidian # Update nevermind is was flake.nix shit <-- Update R.I.P <-- Update WE'RE SO BACK <-- I'm sorry little one, you were too trash for me to try to figure out. https://github.com/NixOS/nixpkgs/issues/302457
     bitwarden  # Update WE'RE SO BACK <-- I believed in you, but you had to be a pain.
         # vscode
     obs-studio
