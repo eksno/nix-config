023886737ec565b9587ea21cadcbe764c9dcb459
Daniel Aanensen <tetochrono@protonmail.com>
2024-09-16 08:46:13 +0200

    feat(teto): dashboard-nvim

---
+++ dotfiles/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,14 @@
+return {
+    -- {
+    -- 'nvimdev/dashboard-nvim',
+    -- event = 'VimEnter',
+    -- config = function()
+    --     require('dashboard').setup {
+    --     -- config
+    --     }
+    -- end,
+    -- dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    -- }
+
+
+}
