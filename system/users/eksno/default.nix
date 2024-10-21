d0116cbca10712827e9ab4d3e641beff6d014fac
Jonas Lindberg <eksno@protonmail.com>
2024-10-21 10:57:41 +0700

    feat(eksno): more groups

--- system/users/eksno/default.nix
+++ system/users/eksno/default.nix
@@ -10,7 +10,7 @@
     users.users.eksno = {
         isNormalUser = true;
         description = "Jonas Lindberg";
-        extraGroups = [ "networkmanager" "wheel" "video" "docker" "input" "uinput" ];
+        extraGroups = [ "networkmanager" "wheel" "video" "docker" "input" "uinput" "audio" "video" "pulse" "storage" "network" "netdev" "games" ];
     };
 
     services.xserver.xkb.layout = "us";
