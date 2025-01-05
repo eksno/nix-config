7a5fbaf174d9f0317ac60f892dec70d16c44f66c
Jonas Lindberg <eksno@protonmail.com>
2025-01-05 18:27:10 +0700

    feat(nvim): disable snacks scroll animation

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.snacks_animate = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
