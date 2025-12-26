7ee44573c59546fbe30b253f3d51107dbf696a42
Jonas Lindberg <eksno@protonmail.com>
2025-12-26 13:23:35 +0700

    fix(fonts): make compatible with newer updates

--- system/lib/fonts.nix
+++ system/lib/fonts.nix
@@ -1,23 +1,29 @@
 { config, pkgs, ... }:
 {
-    fonts.fontDir.enable = true;
-    fonts.packages = with pkgs; [
-        nerd-fonts.fira-code
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-emoji
-        liberation_ttf
-        fira-code
-        fira-code-symbols
-        mplus-outline-fonts.githubRelease
-        dina-font
-        proggyfonts
-    ];
-    fonts.fontconfig = {
-        defaultFonts = {
-            serif = [  "Liberation Serif" "Vazirmatn" ];
-            sansSerif = [ "Ubuntu" "Vazirmatn" ];
-            monospace = [ "Ubuntu Mono" ];
-        };
+  fonts.fontDir.enable = true;
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+  ];
+  fonts.fontconfig = {
+    defaultFonts = {
+      serif = [
+        "Liberation Serif"
+        "Vazirmatn"
+      ];
+      sansSerif = [
+        "Ubuntu"
+        "Vazirmatn"
+      ];
+      monospace = [ "Ubuntu Mono" ];
     };
+  };
 }
