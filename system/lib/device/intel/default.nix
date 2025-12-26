08222bc13a3b119ee833411ed6c48ab7f133cd71
Jonas Lindberg <eksno@protonmail.com>
2025-12-26 13:23:20 +0700

    fix(intel): update intel config to be modern

--- system/lib/device/intel/default.nix
+++ system/lib/device/intel/default.nix
@@ -11,19 +11,21 @@
   boot.kernelParams = [
     "i915.enable_guc=3"
     "i915.enable_psr=2"
-    "i915.enable_fbc=1"
-    "i915.enable_dc=2"
   ];
   powerManagement.powertop.enable = true;
+
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver
-      intel-vaapi-driver
+      # intel-vaapi-driver
       # libvdpau-va-gl
       vpl-gpu-rt
+      intel-compute-runtime # OpenCL for Intel Arc / Iris Xe
+      level-zero # Intel Level Zero
     ];
   };
+
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "iHD";
   }; # Force intel-media-driver
