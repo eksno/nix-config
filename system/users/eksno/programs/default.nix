d5601e6ce7dd951857e247650be30a9caf96d47c
Jonas Lindberg <eksno@protonmail.com>
2026-03-08 02:41:33 +0700

    feat: rip helvum

--- system/users/eksno/programs/default.nix
+++ system/users/eksno/programs/default.nix
@@ -123,7 +123,6 @@
     eww
     waybar
     libreoffice-fresh
-    helvum
     dbeaver-bin
     obsidian # Update nevermind is was flake.nix shit <-- Update R.I.P <-- Update WE'RE SO BACK <-- I'm sorry little one, you were too trash for me to try to figure out. https://github.com/NixOS/nixpkgs/issues/302457
     bitwarden-desktop # Update WE'RE SO BACK <-- I believed in you, but you had to be a pain.
